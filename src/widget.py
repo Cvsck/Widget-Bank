961711858d82b91250be5710697432bd2106e785
unknown <moikrym@mail.ru>
2024-12-18 09:53:31 +0300

    add account

--- src/widget.py
+++ src/widget.py
@@ -1,13 +1,37 @@
-def mask_account_card(account_number: Union[str]) -> Union[str]:
-    print(mask_account_card("Maestro 1596837868705199",
-                            "Счет 64686473678894779589",
-                            "MasterCard 7158300734726758",
-                            "Счет 35383033474447895560",
-                            "Visa Classic 6831982476737658",
-                            "Visa Platinum 8990922113665229",
-                            "Visa Gold 5999414228426353",
-                            "Счет 73654108430135874305""))
+from typing import Union
+
+""" определяем переменную через импорт """
+
+from src.masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(mac_number: Union[str, int]) -> Union[str]:
+    """  Функция принимает список  """
+    name_card = ''
+    number_card = ''
+    number_account = 0
+    for i in mac_number:
+        if i.isalpha():
+            name_card += i  # добавляет если значение буква в пустую строку
+        elif i.isdigit():
+            number_card += str(i)  # добавляет если значение цифра в пустую строку
+            number_account += 1
+
+    if number_account > 16:
+        return f"{name_card} {get_mask_account(number_card)}"
+    elif name_card == "VisaPlatinum":
+        return f"{name_card[0:4]} {name_card[4:]} {get_mask_card_number(number_card)}"
+    else:
+        return f"{name_card} {get_mask_card_number(number_card)}"
+
+
+print(mask_account_card("Visa Platinum 7000792289606361"))
+print(mask_account_card("Счет 73654108430135874305"))
+print(mask_account_card("MasterCard 7158300734726758"))
+print(mask_account_card("Maestro 1596837868705199"))
+
+
 
 
 def get_date(date_number: Union[str]) -> Union[str]:
-    pass
+
