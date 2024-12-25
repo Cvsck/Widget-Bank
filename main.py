f8feb0ba453ac8b961c31162df7d3dd3f90129d4
main <moikrym@mail.ru>
2024-12-25 17:44:53 +0300

    add docstring

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from src import masks
 
 from src import widget
 
+from src import processing
+
+from src.processing import sorted_list
+
 print(masks.get_mask_card_number("7000792289606361"))
 
 print(masks.get_mask_account("73654108430135874305"))
@@ -16,3 +20,5 @@ print(widget.mask_account_card("MasterCard 7158300734726758"))
 print(widget.mask_account_card("Maestro 1596837868705199"))
 
 print(widget.get_date("2024-03-11T02:26:18.671407"))
+
+print(processing.filter_by_state(sorted_list))
--- src/processing.py
+++ src/processing.py
@@ -1,32 +1,35 @@
-from typing import List, Any
+import typing
 
 
-def filter_by_state(my_lists, state='EXECUTED') -> Any:
-    new_list = []
+def filter_by_state(my_lists: [str], state: [str] = 'EXECUTED') -> typing.Any:
+    """Функция возвращает новый список словарей"""
+
+    new_list_dic = []
 
     for dat, stat in enumerate(my_lists):
-        if stat.get('state') == state:
-            new_list.append(stat)
+        if stat.get('state') == state:  # основное условия создание нового списка
+            new_list_dic.append(stat)
 
-    return new_list
+    return new_list_dic
 
 
-def sort_by_date(new_date, key=None) -> Any:
+def sort_by_date(new_date: [typing.Any], key: [typing.Any] = None) -> typing.Any:
+    """Функция возвращает новый список, отсортированный по дате"""
+
     if not key:
-        key = 'date'
+        key = 'date'  # основное условия создание нового списка
     sorted_data = sorted(new_date, key=lambda x: x.get(key), reverse=True)
 
     return sorted_data
 
 
-my_list = [
+list_of_dictionaries = [
     {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
     {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
     {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
     {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
 ]
 
-filtered_list = filter_by_state(my_list, state='EXECUTED')
-sorted_list = sort_by_date(filtered_list)
+filtered_list = filter_by_state(list_of_dictionaries, state='EXECUTED')  # аргументы передаются в первую функцию
 
-print(sorted_list)
+sorted_list = sort_by_date(filtered_list)  # аргумент принимает вторая функцию на основе полученных данных из первой
