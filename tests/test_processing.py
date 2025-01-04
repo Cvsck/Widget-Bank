1fe361784b983833c48f3e9d0fade300a1a8d815
main <moikrym@mail.ru>
2025-01-04 12:14:26 +0300

    add conftest

---
+++ tests/test_processing.py
@@ -0,0 +1,7 @@
+from src.processing import filter_by_state, list_of_dictionaries, sort_by_date, sorted_list
+
+
+#def test_filter_by_state(new_list_dic=None):
+    #assert filter_by_state(list_of_dictionaries) == new_list_dic
+#def test_sort_by_date():
+    #assert sort_by_date(sorted_list) == [{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'}, {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}]
