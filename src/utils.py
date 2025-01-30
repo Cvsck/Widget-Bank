d0dede20659cc8859125cc347feea75eebc14254
main <moikrym@mail.ru>
2025-01-30 17:16:32 +0300

    add new currency conversion

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+import json
+import os
+from typing import Any
+
+
+def get_financial_transactions(path: str) -> list[Any] | Any:
+    if not os.path.exists(path):
+        return []
+    with open(path, encoding="utf-8") as file_json:
+        data_json = json.load(file_json)
+    return data_json
+
+
+transactions = get_financial_transactions("C:\\Users\\Макс\\my_prj\\bank widget\\data\\operations.json")
+
+print(transactions)
